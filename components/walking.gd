19fff6e40e718b51bc59bebe8e8f25541e658752
Alex <der.urheber@gmail.com>
2024-09-05 02:01:41 +0300

    bg + music

--- components/walking.gd
+++ components/walking.gd
@@ -1,13 +1,19 @@
 extends StateComponent
-
+@onready var moveable = owner.get_node("moveable")
+@onready var direction = owner.get_node("direction")
 # Called when the node enters the scene tree for the first time.
+var active = 0
 func perform(vars):
-	var direction = owner.get_node("direction")
-	var moveable = owner.get_node("moveable")
 	if vars["direction"] != direction.direction:
 		direction.change_direction()
+	active = 1
 	moveable.go()
 
 func stop():
 	var moveable = owner.get_node("moveable")
+	active = 0
 	moveable.stop()
+
+func _physics_process(delta):
+	if active:
+		moveable.go()
